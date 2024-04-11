dfcbd28e9e9cd52605b47fbff0dbc9937bed695e
Luis Yanez <yanezblancoluis@gmail.com>
2024-04-10 23:51:22 -0400

    test update

--- tests/Feature/Http/Controllers/RecipeControllerTest.php
+++ tests/Feature/Http/Controllers/RecipeControllerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Recipe;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -11,9 +12,11 @@ use Laravel\Sanctum\Sanctum;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
+use Illuminate\Http\UploadedFile;
+
 class RecipeControllerTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     public function test_index(){
         Sanctum::actingAs(User::factory()->create());
@@ -54,6 +57,51 @@ class RecipeControllerTest extends TestCase
                     'attributes' => ['title', 'description'], // resto de campos
                 ]
             ]);
+    }
+
+    public function test_store(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        $category = Category::factory()->create();
+        $tag = Tag::factory()->create();
+
+        $data = [
+            'category_id'  => $category->id,
+            'title'        => $this->faker->sentence,
+            'description'  => $this->faker->paragraph,
+            'ingredients'  => $this->faker->text,
+            'instructions' => $this->faker->text,
+            'tags'         => $tag->id,
+            'image'        => UploadedFile::fake()->image('recipe.png')
+        ];
+
+        $response = $this->postJson('/api/recipes/', $data);
+        $response->assertStatus(Response::HTTP_CREATED);
+    }
+
+    public function test_update(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        $category = Category::factory()->create();
+        $recipe = Recipe::factory()->create();
+
+        $data = [
+            'category_id'  => $category->id,
+            'title'        => 'updated title',
+            'description'  => 'updated description',
+            'ingredients'  => $this->faker->text,
+            'instructions' => $this->faker->text,
+        ];
+
+        $response = $this->putJson('/api/recipes/' . $recipe->id, $data);
+        $response->assertStatus(Response::HTTP_OK);
+
+        $this->assertDatabaseHas('recipes', [
+            'title' => 'updated title',
+            'description' => 'updated description'
+        ]);
     }
 
     public function test_destroy(): void
